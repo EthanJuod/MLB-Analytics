3dcc6471cef25f1e916fdf65ada616224c4adb43
EthanJuod <156048207+EthanJuod@users.noreply.github.com>
2026-05-05 00:09:03 -0400

    Update queries.py

--- MLB-Analytics/queries.py
+++ MLB-Analytics/queries.py
@@ -7,9 +7,11 @@ def batting_average(season=None):
     cursor = conn.cursor()
 
     if season:
-        row = cursor.execute("SELECT player_name, (hits * 1.0 / NULLIF(at_bats, 0)) AS avg FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id WHERE season = ? ORDER BY avg DESC;", (season,)).fetchall()
+        row = cursor.execute("SELECT player_name, (hits * 1.0 / NULLIF(at_bats, 0)) AS avg FROM Players AS p JOIN Stats AS s " \
+        "ON p.player_id = s.player_id WHERE season = ? ORDER BY avg DESC;", (season,)).fetchall()
     else:
-        row = cursor.execute("SELECT player_name, (SUM(hits) * 1.0 / NULLIF(SUM(at_bats), 0)) AS avg FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id GROUP BY p.player_id, p.player_name ORDER BY avg DESC;").fetchall()
+        row = cursor.execute("SELECT player_name, (SUM(hits) * 1.0 / NULLIF(SUM(at_bats), 0)) AS avg FROM Players AS p JOIN Stats AS s " \
+        "ON p.player_id = s.player_id GROUP BY p.player_id, p.player_name ORDER BY avg DESC;").fetchall()
 
     conn.close()
 
@@ -20,9 +22,11 @@ def top_homerun_hitters(season=None):
     cursor = conn.cursor()
 
     if season:
-        row = cursor.execute("SELECT player_name, home_runs FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id WHERE season = ? ORDER BY home_runs DESC LIMIT 10;", (season,)).fetchall()
+        row = cursor.execute("SELECT player_name, home_runs FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id " \
+        "WHERE season = ? ORDER BY home_runs DESC LIMIT 10;", (season,)).fetchall()
     else: 
-        row = cursor.execute("SELECT player_name, SUM(home_runs) AS home_runs AS total_hrs FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id GROUP BY p.player_id, p.player_name ORDER BY total_hrs DESC LIMIT 10;").fetchall()
+        row = cursor.execute("SELECT player_name, SUM(home_runs) AS home_runs AS total_hrs FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id " \
+        "GROUP BY p.player_id, p.player_name ORDER BY total_hrs DESC LIMIT 10;").fetchall()
 
     conn.close()
     return row
@@ -32,9 +36,11 @@ def player_comparison(player1, player2, season=None):
     cursor = conn.cursor()
 
     if season:
-        row = cursor.execute("SELECT player_name,(hits * 1.0 / NULLIF(at_bats, 0)) AS avg, hits, home_runs, rbi, games FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id WHERE (player_name = ? OR player_name = ?) AND season = ?;", (player1, player2, season)).fetchall()
+        row = cursor.execute("SELECT player_name,(hits * 1.0 / NULLIF(at_bats, 0)) AS avg, hits, home_runs, rbi, games " \
+        "FROM Players AS p JOIN Stats AS s ON p.player_id = s.player_id WHERE (player_name = ? OR player_name = ?) AND season = ?;", (player1, player2, season)).fetchall()
     else:
-        row = cursor.execute("SELECT player_name, SUM(at_bats) AS at_bats, (SUM(hits) * 1.0 / NULLIF(SUM(at_bats), 0)) AS avg, SUM(hits) AS hits, SUM(home_runs) AS home_runs, SUM(rbi), SUM(games) AS GAMES FROM Players as p JOIN Stats AS s On p.player_id = s.player_id WHERE player_name = ? or player_name = ? GROUP BY p.player_id, p.player_name", (player1, player2)).fetchall()
+        row = cursor.execute("SELECT player_name, SUM(at_bats) AS at_bats, (SUM(hits) * 1.0 / NULLIF(SUM(at_bats), 0)) AS avg, SUM(hits) AS hits, SUM(home_runs) AS home_runs, SUM(rbi), SUM(games) AS games " \
+        "FROM Players as p JOIN Stats AS s On p.player_id = s.player_id WHERE player_name = ? or player_name = ? GROUP BY p.player_id, p.player_name", (player1, player2)).fetchall()
 
     conn.close()
     return row
